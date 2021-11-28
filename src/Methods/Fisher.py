344c7fa4b401b60d3e32935e85189dace0f707df
calebhadley1 <caleb@hadleynet.org>
2021-11-27 21:48:29 -0500

    Path fixes

--- src/Methods/Fisher.py
+++ src/Methods/Fisher.py
@@ -43,7 +43,7 @@ def occurenceModel(scene):
     # A(o, S ) is a function which takes an object o positioned within a scene S and returns an unnormalized probability of its current placement and orientation.
     # (describes where those objects can be placed)
 
-def sunRGBDDataMiningFisher(starting_location = None,data_cleanup = None, write_type = 'w'):
+def sunRGBDDataMiningFisher():
     '''max_amount controls the maximum number of rooms we look out, which helps us bound the problem
        starting_location tells us that there are rooms in the beginning that we can skip, most likely because we've already looked at them
        location_amount tells us to only look at a certain number of rooms, again so that we can run this in stages
@@ -51,8 +51,8 @@ def sunRGBDDataMiningFisher(starting_location = None,data_cleanup = None, write_
        min_amount is another room remover. We determine a room threshold where we believe that we cannot get good data under the threshold
        write_type tells us if we are overwriting the file or appending.
     '''
-    import SUNRGBD
-    import NYUSupport
+    import src.Parse.SUNRGBD as SUNRGBD
+    import src.Parse.NYUSupport as NYUSupport
 
     NYUSupport.mineNYU2Data(path_to_data,"support_mining.csv")
     frames = defaultdict(list)
@@ -67,3 +67,11 @@ def sunRGBDDataMiningFisher(starting_location = None,data_cleanup = None, write_
         frames = SUNRGBD.getFrames(d,paths,frames)
     #This combines our similar rooms from the pattern analysis
     print ("Total Objects",len(getObjects(frames)))
+
+if __name__ == "__main__":
+    import os
+    same_rooms   = {"idk":"corridor","recreation_room":"rest_space"}
+    same_objects = {"fridge":"refridgerator","bathroomvanity":"bathroom_vanity","toyhouse":"toy_house","bookshelf":"book_shelf","tissuebox":"tissue_box"}
+    removed_rooms = ["Dining_Room_Garage_Gym","Dining_Room_Kitchen_Office_Garage","Room","Living_Room_Dining_Room_Kitchen_Garage"]
+    support = (20,1000)
+    sunRGBDDataMiningFisher()
