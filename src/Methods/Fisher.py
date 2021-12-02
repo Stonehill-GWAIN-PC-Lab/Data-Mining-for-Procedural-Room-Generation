59988b79307203cca8dde51ccc61140044423f47
calebhadley1 <caleb@hadleynet.org>
2021-12-02 11:18:57 -0500

    using fisher cost function5

--- src/Methods/Fisher.py
+++ src/Methods/Fisher.py
@@ -215,18 +215,9 @@ def arrangementModel(object, scene):
     
 def twoObjectRelationshipProbability(obj1,obj2, value_array = None):
     #basically finding the zscore of the relationship
-    print('printing obj1')
-    print(obj1)
-    print(obj1.centroid)
-    print('printing obj1')
-    print(obj2)
-    print(obj2.centroid)
-    print()
     std=15.0
     mean=90.0
-    #distance = np.sum(abs(obj1.centroid - obj2.centroid))
-    distance = np.sum((obj1.centroid-obj2.centroid)**2) #d= sqrt((x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2)
-    print("distance:", distance)
+    distance = np.sqrt(np.sum((obj1.centroid-obj2.centroid)**2)) #d=sqrt((x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2)
     #print(distance)
     z_score = (distance-mean)/std
     #print(z_score)
