5e0518a1271fb9dea6213e276784339167cbe1ec
James Milizebra <milinazzojames@gmail.com>
2021-12-03 02:15:11 -0500

    relationship dataframe

--- src/Methods/Fisher.py
+++ src/Methods/Fisher.py
@@ -68,11 +68,10 @@ def buildRelationshipDataFrame(df):
     #row_mask7 = df["edge 1"]=="null"
     #row_mask8 = df["edge cost"]=="null"
     #print(df[row_mask1][row_mask2][row_mask3])#[row_mask4][row_mask5][row_mask6][row_mask7][row_mask8])
-    m = df.index==0
+    m = df.index=='0'
     for x in m:
         if x == True:
             print(x)
-    print(df.index)
     #splitting up the scenes
     #for i in range(0,341,1):
         #mask = 
